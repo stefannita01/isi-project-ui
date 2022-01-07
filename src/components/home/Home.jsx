4897c625a026229344aa434e7baa4a884cffb4c5
stefanNita <stefannita01@gmail.com>
2022-01-07 19:06:59 +0200

    button top left to zoom to your location :o

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,41 @@
+import React, { useRef, useEffect } from "react";
+import Locate from "@arcgis/core/widgets/Locate";
+import Track from "@arcgis/core/widgets/Track";
+import Graphic from "@arcgis/core/Graphic";
+import MapView from "@arcgis/core/views/MapView";
+import WebMap from "@arcgis/core/WebMap";
+
+import styles from "./Home.module.css";
+
 const Home = () => {
-  return (
-    <>
-      <p>Congrats, you are logged in</p>
-    </>
-  );
+  const mapDiv = useRef(null);
+
+  useEffect(() => {
+    if (mapDiv.current) {
+      const webmap = new WebMap({
+        basemap: "streets-vector",
+      });
+
+      const view = new MapView({
+        container: mapDiv.current,
+        map: webmap,
+      });
+
+      const locate = new Locate({
+        view: view,
+        useHeadingEnabled: false,
+        goToLocationEnabled: true,
+        goToOverride: function (view, options) {
+          options.target.scale = 2000;
+          return view.goTo(options.target);
+        },
+      });
+
+      view.ui.add(locate, "top-left");
+    }
+  }, []);
+
+  return <div className={styles.mapDiv} ref={mapDiv}></div>;
 };
 
 export default Home;
